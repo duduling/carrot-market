3dcf42a56e1bd3c629db3ee0ab40059f416754f3
Duduling <duduling20@gmail.com>
2022-08-09 17:50:03 +0900

    #5.5 Upload Item

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,70 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const ItemDetail: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-5 py-10">
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4  border-b pb-5 cursor-pointer justify-between"
-        >
-          <div className="flex space-x-4">
-            <div className="w-20 h-20 bg-gray-400 rounded-md" />
-            <div className="pt-2 flex flex-col">
-              <h3 className="text-sm font-medium text-gray-900">
-                New iPhone 14
-              </h3>
-              <span className="text-xs text-gray-500">Black</span>
-              <span className="font-medium mt-1 text-gray-900">$95</span>
-            </div>
+    <div className="px-4 py-10">
+      <div className="mb-8">
+        <div className="h-96 bg-slate-300" />
+        <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
+          <div className="w-12 h-12 rounded-full bg-slate-300" />
+          <div>
+            <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
+            <p className="text-xs font-medium text-gray-500">
+              View profile &rarr;
+            </p>
           </div>
-          <div className="flex space-x-2 items-end justify-end">
-            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
+        </div>
+        <div className="mt-5">
+          <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+          <span className="text-3xl block mt-3 text-gray-900">$140</span>
+          <p className=" my-6 text-gray-700">
+            My money&apos;s in that office, right? If she start giving me some
+            bullshit about it ain&apos;t there, and we got to go someplace else
+            and get it, I&apos;m gonna shoot you in the head then and there.
+            Then I&apos;m gonna shoot that bitch in the kneecaps, find out where
+            my goddamn money is. She gonna tell me too. Hey, look at me when
+            I&apos;m talking to you, motherfucker. You listen: we go in there,
+            and that ni**a Winston or anybody else is in there, you the first
+            motherfucker to get shot. You understand?
+          </p>
+          <div className="flex items-center justify-between space-x-2">
+            <button className="flex-1 bg-orange-500 text-white py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 font-medium hover:bg-orange-600 focus:ring-orange-500 ">
+              Talk to seller
+            </button>
+            <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
               <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
+                className="h-6 w-6 "
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
+                />
               </svg>
-              <span>1</span>
-            </div>
-            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>1</span>
-            </div>
+            </button>
           </div>
         </div>
-      ))}
-      <button className="fixed hover:bg-orange-500 transition-colors cursor-pointer  bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-      </button>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
+        <div className=" mt-6 grid grid-cols-2 gap-4">
+          {[1, 2, 3, 4, 5, 6].map((_, i) => (
+            <div key={i}>
+              <div className="h-56 w-full mb-4 bg-slate-300" />
+              <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
+              <span className="text-sm font-medium text-gray-900">$6</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default ItemDetail;
