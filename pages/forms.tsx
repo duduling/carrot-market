6aa440d387ff6d22a0103296f174af37003a7c14
Duduling <duduling20@gmail.com>
2022-08-23 15:41:23 +0900

    #7.4 Validation part Two

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,26 +1,48 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
-// Less code (c)
 // Better validation
 // Better Erros (set, clear, display)
 // Have control over inputs
-// Dont deal with events (c)
-// Easier Inputs (c)
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onVaild = (): void => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onVaild = (data: LoginForm): void => {
     console.log("Hi, I'm vaild");
   };
+
+  const onInvaild = (errors: FieldErrors): void => {
+    console.log(errors);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onVaild)}>
+    <form onSubmit={handleSubmit(onVaild, onInvaild)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            value: 5,
+            message: "The username should be longer than 5 chars.",
+          },
+        })}
         type="text"
         placeholder="Username"
       />
-      <input {...register("email")} type="email" placeholder="Email" />
-      <input {...register("password")} type="password" placeholder="Password" />
+      <input
+        {...register("email", { required: "Username is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "Password is required" })}
+        type="password"
+        placeholder="Password"
+      />
       <input type="submit" value="Create Account" />
     </form>
   );
