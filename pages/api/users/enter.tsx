8e57226c967320aae3883caebcf1b73d486e812b
Duduling <duduling20@gmail.com>
2022-09-09 15:25:23 +0900

    #9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,8 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
