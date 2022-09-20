1fe5fed279767438db170cd11512219ff5d14a99
Duduling <duduling20@gmail.com>
2022-09-20 22:44:26 +0900

    #9.8 Profile Handler

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -3,6 +3,14 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withIronSessionApiRoute } from "./../../../node_modules/iron-session/next/index";
 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -13,10 +21,10 @@ async function handler(
       payload: token,
     },
   });
-  if (!exists) res.status(404).end();
+  if (!exists) return res.status(404).end();
 
   req.session.user = {
-    id: exists?.userId,
+    id: exists.userId,
   };
   await req.session.save();
   res.status(200).end();
