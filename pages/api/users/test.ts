882bdfca23a1a1e253885be82ee5f89d87200b39
Duduling <duduling20@gmail.com>
2023-02-23 22:27:59 +0900

    #11.5 Related Products

---
+++ pages/api/users/test.ts
@@ -0,0 +1,11 @@
+function countRegexMatches(target: string | null, regexString: string): number {
+  if (typeof target === "/* implement Answer 1 */") {
+    let result = target["/* implement Answer 2 */"](
+      new RegExp(regexString, "g")
+    );
+    if (result !== null) {
+      return /* implement Answer 3 */;
+    }
+  }
+  return 0;
+}
